d790f4c8cf9883ad76c209cf76a19351d6e69d0f
Charles Lao <charles.h.lao@gmail.com>
2025-01-22 23:53:04 +1300

    Fix DailyTask repository

--- Repository/IRepository/IDailyTaskRepository.cs
+++ Repository/IRepository/IDailyTaskRepository.cs
@@ -10,7 +10,7 @@ namespace PokeGoTools.Repository.IRepository
         public Task<DailyTask> UpdateAsync(DailyTask obj);
         public Task<bool> CompleteTaskAsync(int id);
         public Task<bool> DeleteAsync(int id);
-        public Task<IEnumerable<DailyTask>> GetAllAsync();
+        public Task<IEnumerable<DailyTask>> GetAllAsync(string? userId);
 
     }
 }
